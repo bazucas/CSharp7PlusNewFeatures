0e1e18cc601f331277443e283b00b27955c91572
Luis Inacio <lrs.inacio@gmail.com>
2021-12-26 14:32:35 +0000

    Deconstruction

---
+++ Features/Deconstruction.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console;
+
+namespace NewFeatures;
+
+public class Demo
+{
+    public static void Main(string[] args)
+    {
+        var myPoint = new Point();
+        var (x, _) = myPoint;
+        WriteLine($"{x}");
+    }
+}
+
+public class Point
+{
+    public int X, Y;
+
+    // deconstruction
+    public void Deconstruct(out int x, out int y)
+    {
+        x = X;
+        y = Y;
+    }
+}
+
