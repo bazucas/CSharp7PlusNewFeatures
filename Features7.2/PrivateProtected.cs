3a812f24363d7a988c931f5b76d2d1ac50bcaa9a
Luis Inacio <lrs.inacio@gmail.com>
2021-12-26 21:03:39 +0000

    Private Protected Acced Modifier and remaining proj files

---
+++ Features7.2/PrivateProtected.cs
@@ -0,0 +1,29 @@
+﻿namespace Features7._2;
+
+public class Base
+{
+    private int a;
+    protected internal int b; // derived classes or classes in same assembly
+    private protected int c;  // containing class or derived classes in same assembly only 
+}
+
+class Derived : Base
+{
+    public Derived()
+    {
+        c = 333; // fine
+
+        b = 3; // no
+    }
+}
+
+class Foo
+{
+    static void Main(string[] args)
+    {
+        Base pp = new Base();
+        var d = new Derived();
+        d.b = 3;
+        // d.c is a no-go
+    }
+}
