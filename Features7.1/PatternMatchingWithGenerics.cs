ff9188e565b71c1895f97f00158910698624be56
Luis Inacio <lrs.inacio@gmail.com>
2021-12-26 20:49:32 +0000

    Pattern Matching with Generics

---
+++ Features7.1/PatternMatchingWithGenerics.cs
@@ -0,0 +1,44 @@
+﻿using static System.Console;
+
+namespace Features7._1;
+
+public class Animal
+{
+
+}
+
+public class Pig : Animal
+{
+
+}
+
+public class PatternMatchingWithGenerics
+{
+    public static void Cook<T>(T animal)
+        where T : Animal
+    {
+        // note the red squiggly!
+        // cast is redundant here
+        if ((object)animal is Pig pig)
+        {
+            // cook and eat it
+            Write("We cooked and ate the pork...");
+        }
+
+        switch (/*(object)*/animal)
+        {
+            case Pig pork:
+                WriteLine(" and it tastes delicious!");
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Need to fall back to C# 7 for this.
+    /// </summary>
+    static void Main(string[] args)
+    {
+        var pig = new Pig();
+        Cook(pig);
+    }
+}
