081a98230ff0e3dcfe9d1d3c78f50d188c8ac53a
Luis Inacio <lrs.inacio@gmail.com>
2021-12-26 15:03:10 +0000

    Expression Bodied Members

---
+++ Features/ExpressionBodiedMembers.cs
@@ -0,0 +1,17 @@
+﻿namespace Features7;
+
+public class Person
+{
+    private int id;
+
+    private static readonly Dictionary<int, string> names = new Dictionary<int, string>();
+
+    public Person(int id, string name) => names.Add(id, name);
+    ~Person() => names.Remove(id);
+
+    public string Name
+    {
+        get => names[id];
+        set => names[id] = value;
+    }
+}
