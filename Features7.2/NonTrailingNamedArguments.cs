d6483147188c5c996472afb8cf54fd2b90216399
Luis Inacio <lrs.inacio@gmail.com>
2021-12-26 21:06:54 +0000

    Non Trailing Named Arguments

---
+++ Features7.2/NonTrailingNamedArguments.cs
@@ -0,0 +1,22 @@
+﻿namespace Features7._2;
+
+class NonTrailingNamedArguments
+{
+    static void doSomething(int foo, int bar)
+    {
+
+    }
+
+    static void Main(string[] args)
+    {
+        doSomething(foo: 33, 44);
+        doSomething(foo: 33, bar: 44);
+        doSomething(33, 44);
+        doSomething(33, bar: 44);
+        doSomething(bar: 33, foo: 44);
+
+        // still illegal
+        //doSomething(33, foo:44)
+        //doSomething(bar: 33, 44);
+    }
+}
