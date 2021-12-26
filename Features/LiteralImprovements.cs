107d16196e08812fbc96e7fea72cab1ac0e27df5
Luis Inacio <lrs.inacio@gmail.com>
2021-12-26 15:14:27 +0000

    Literal Improvements

---
+++ Features/LiteralImprovements.cs
@@ -0,0 +1,19 @@
+﻿namespace Features7;
+
+public class LiteralImprovements
+{
+    static void MainLI(string[] args)
+    {
+        int a = 123_321;
+        int b = 123_321______123;
+
+        // cannot do trailing
+        //int c = 1_2_3___; // R# remove
+
+        // also works for hex
+        long h = 0xAB_BC_D123EF;
+
+        // also binay
+        var bin = 0b1110_0010_0011;
+    }
+}
