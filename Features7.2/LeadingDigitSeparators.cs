861b490c449d6e235df90dcf0e8b32bd281f9a80
Luis Inacio <lrs.inacio@gmail.com>
2021-12-26 20:56:43 +0000

    Leading Underscores Numeric Separators

---
+++ Features7.2/LeadingDigitSeparators.cs
@@ -0,0 +1,13 @@
+﻿namespace Features7._2;
+
+public class LeadingUnderscoresNumericSeparators
+{
+    static void Main(string[] args)
+    {
+        // binary
+        var x = 0b_1111_0000;
+
+        // hex
+        var y = 0x_baad_f00d;
+    }
+}
